76dd1702deb837ce3a5d91bdbaad1a1c0237fb90
mitchellkwong <mitchellkwong@gmail.com>
2020-11-14 17:36:14 +0800

    Bumped splash page to UI specifications

--- student_portal/src/components/Register.vue
+++ student_portal/src/components/Register.vue
@@ -1,7 +1,6 @@
 <template>
 	<div id="register">
-		<AppNav></AppNav>	
-		<AppHeader></AppHeader>
+		<AppHeader show-image=True></AppHeader>
 		<h2>Register now!</h2>	
 		<label>Username</label> <input type="text" name="user" v-model="username"><br>
 		<label>Password</label> <input type="text" name="pass" v-model="password"><br>
@@ -11,7 +10,6 @@
 </template>
 
 <script>
-import AppNav from './app/Nav.vue'
 import AppHeader from './app/Header.vue'
 import AppFooter from './app/Footer.vue'
 import database from '../firebase.js'
@@ -19,7 +17,6 @@ import database from '../firebase.js'
 export default {
 	name: 'Register',
 	components: {
-		AppNav,
 		AppHeader,
 		AppFooter,
 	},
--- student_portal/src/components/app/Header.vue
+++ student_portal/src/components/app/Header.vue
@@ -1,24 +1,33 @@
 <template>
 	<div id="appHeader" >
-		<p v-if="user">User "{{ user }}" is logged in.</p>
+		<AppNav :user="user" :style="{'background-color':color}"></AppNav>
 		<div class="jumbotron" :style="{'background-color':color}" >
 			<h1 class="display-4">H A L C Y O N</h1>
 			<p class="lead">Changing the way students work from home.</p>
-			<hr class="my-4">
-			<div style="text-align: center">
-			<a class="btn btn-primary btn-lg" href="/login" role="button">Log In</a>
-			<div class="divider"/>
-			<a class="btn btn-primary btn-lg" href="/register" role="button">Register Now</a>
-			</div>
+			<img id="thumbnail"
+				v-if="showImage"
+				v-img :src="require('@/assets/graduation.png')" 
+				width="250"
+			>
+			<!-- <hr class="my-4">
+				<div style="text-align: center">
+				<router-link class="btn btn-primary btn-lg" to="/login" role="button">Log In</router-link>
+				<div class="divider"/>
+				<router-link class="btn btn-primary btn-lg" to="/register" role="button">Register Now</router-link>
+			</div> -->
 		</div>
 	</div>
 </template>
 
 <script type="text/javascript">
+import AppNav from './Nav.vue'
+
 export default {
 	name: 'AppHeader',
-	props: ['user'],
-	components: {},
+	props: ['user', 'showImage'],
+	components: {
+		AppNav,
+	},
 	data: function() {
 		return{
 			color:"#AFEEEE"
@@ -28,12 +37,17 @@ export default {
 </script>
 
 <style lang="css" scoped>
+#thumbnail {
+	margin-bottom: -125px;
+}
+
 #appHeader {
 	
 }
 
-#jumbotron {
-
+.jumbotron {
+	padding-bottom: 0px;
+	margin-bottom: 157px; 
 }
 
 .divider{
