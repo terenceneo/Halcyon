3940eec045bf04c48d43649fef367d3d18582fd7
mitchellkwong <mitchellkwong@gmail.com>
2020-11-15 06:16:26 +0800

    Improved input validation

--- student_portal/src/components/user/Modules.vue
+++ student_portal/src/components/user/Modules.vue
@@ -38,19 +38,20 @@ export default {
 	},
 	methods: {
 		addModule(moduleCode) {
+			moduleCode = moduleCode.toUpperCase();
 			if (this.moduleList.some(entry => entry.moduleCode == moduleCode)) {
-				alert('Module already added');
+				alert(moduleCode+' already added!');
 				this.moduleCode = null;
 			} else {
 				let url = 'https://api.nusmods.com/v2/2019-2020/modules/'+moduleCode+'.json'
 				request.get(url, { json: true }, (err, res, body) => {
 					if (res.statusCode != 200) {
-						alert('Invalid module code');
+						alert(moduleCode+' is not a module!');
 						this.moduleCode = null;
 					} else {
 						this.moduleList.push(body);
 						db.collection('users').doc(this.user).update({modules: this.moduleList});
-						alert('Added '+this.moduleCode+'!');
+						alert('Added '+moduleCode+'!');
 						this.moduleCode = null;
 					}
 				});
