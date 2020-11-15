cff7941b872e535c567015ea448483cecc615a07
Terence Neo <terencenyy@yahoo.com.sg>
2020-11-15 18:53:47 +0800

    Retrieve and display exams

--- student_portal/src/components/user/Home.vue
+++ student_portal/src/components/user/Home.vue
@@ -3,8 +3,18 @@
 		<h1>Home Page</h1>
 		<div>
 			<h2>Alerts</h2>
+			<p><i>Note: for testing purposes, we will assume that the sem has not ended and lessons are as timetabled</i></p>
 			<ul class="list-group list-group-flush" v-for="lesson in timetable" :key="lesson.moduleCode">
-				<li class="list-group-item">{{ lesson.startTime }} - {{ lesson.moduleCode }} {{ lesson.lessonType }} </li>
+				<li class="list-group-item">
+					{{ lesson.startTime }} - {{ lesson.moduleCode }} {{ lesson.lessonType }}
+				</li>
+				<!-- <li class="list-group-item">GES1010 Assignment 1 Due</li> -->
+				<!-- <li class="list-group-item">DBA 2705 Assignment 2 Due</li> -->
+			</ul>
+			<ul class="list-group list-group-flush" v-for="exam in exams" :key="exam.moduleCode">
+				<li class="list-group-item">
+					{{ exam.examDate }} - {{ exam.moduleCode }} {{ exam.title }}
+				</li>
 				<!-- <li class="list-group-item">GES1010 Assignment 1 Due</li> -->
 				<!-- <li class="list-group-item">DBA 2705 Assignment 2 Due</li> -->
 			</ul>
@@ -18,6 +28,25 @@ export default {
 	name: 'Home',
 	props: ['user', 'username', 'moduleList', 'timetable'],
 	components: {},
+	computed: {
+		exams: function() {
+			let exams = [];
+			this.moduleList.forEach(mod => {
+				mod.semesterData
+					.filter(sem => sem.semester == '2')
+					.filter(sem => sem.examDate != null)
+					.forEach(sem => {
+						exams.push({
+							moduleCode: mod.moduleCode,
+							title: mod.title,
+							examDate: sem.examDate,
+							examDuration: sem.examDuration
+						});
+					});
+				});
+			return exams;
+		}
+	},
 }
 </script>
 
