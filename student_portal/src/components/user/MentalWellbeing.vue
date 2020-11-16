0947e41918b9edf7927894722a89e8197bc4fea8
zhihantan <70834772+zhihantan@users.noreply.github.com>
2020-11-16 14:45:37 +0800

    Revert "Merge pull request #44 from urlydunknwme/master"
    This reverts commit 94d1254f178eea8be255ecdd8dabdb10bca7db4b, reversing
    changes made to 296edd7ef6cbeeaef620c8cc69e12be495903766.

--- student_portal/src/components/user/MentalWellbeing.vue
+++ student_portal/src/components/user/MentalWellbeing.vue
@@ -1,24 +1,7 @@
 <template>
 	<div id='mentalWellbeing'>
-		<h1>Feeling stressed during the pandemic?</h1>
-		
-		<img src="@/assets/covid-anxiety.jpg">
-
-		<h2>Check out our selection of articles below.</h2>
-    <div class="articles">
-      <figure>
-        <a href="https://www.nm.org/about-us/northwestern-medicine-newsroom/nm-news-blog/13-ways-to-help-a-friend-with-anxiety-during-coronavirus"><img src="@/assets/call.jpg"></a>
-        <figcaption>Call a friend</figcaption>
-      </figure>
-      <figure>
-        <a href="https://www.huffpost.com/entry/benefits-of-journaling-_b_6648884"><img src="@/assets/journal.png"></a>
-        <figcaption>Benefits of journalling</figcaption>
-      </figure>
-      <figure>
-        <a href="https://www.triathlete.com/training/yoga-during-the-pandemic/"><img src="@/assets/yoga.jpg"></a>
-        <figcaption>Pick up yoga</figcaption>
-      </figure>
-    </div>
+		<h1>Time for a break</h1>
+		<p>User "{{ user }}" is logged in.</p>
 	</div>
 </template>
 
