a6594557d22e8601b4e0dd4cbda1d7c80ccb74ed
Terence Neo <terencenyy@yahoo.com.sg>
2020-11-16 17:55:59 +0800

    Fix infinite loop

--- student_portal/src/components/user/Calendar.vue
+++ student_portal/src/components/user/Calendar.vue
@@ -14,17 +14,15 @@
 						</tr>
 					</thead>
 
-					<tbody v-for="(lesson, index) in timetable" :key="index">
+					<tbody v-for="(lesson, index) in timetableRowSpan" :key="index">
 						<tr>
 							<!-- Note: edit rowspan for multiple items in a day -->
-							<td v-if="lesson.countdown != prevCountdown" class="agenda-date" rowspan="1">
+							<td v-if="lesson.showDate" class="agenda-date" :rowspan="rowSpanDate[lesson.countdown]">
 								<div class="dayofmonth">{{ future(lesson.countdown).getDate() }}</div>
 								<div class="dayofweek">{{ lesson.day }}</div>
 								<div class="shortdate text-muted">
 									{{ months[future(lesson.countdown).getMonth()] }}, {{ future(lesson.countdown).getFullYear() }}
 								</div>
-							</td>
-							<td v-else>
 							</td>
 							<td class="agenda-time">
 								{{ lesson.startTime }} - {{ lesson.endTime }}
@@ -53,17 +51,43 @@ export default {
 	data: function() {
 		return {
 			months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-			prevCountdown: null, // to prevent same dates from repeating
+			// prevCountdown: null, // to prevent same dates from repeating
 		}
 	},
 	methods: {
 		future: function(incr) {
 			let future = new Date()
 			future.setDate(this.today.getDate() + incr);
-			this.prevCountdown = incr; // to prevent same dates from repeating
+			// this.prevCountdown = incr; // to prevent same dates from repeating
 			return future;
 		},
 	},
+	computed: {
+		rowSpanDate: function() {
+			let rowSpans = {};
+			for (let index in this.timetable) {
+				if (this.timetable[index].countdown in rowSpans) {
+					rowSpans[this.timetable[index].countdown]++;
+				} else {
+					rowSpans[this.timetable[index].countdown] = 1;
+				}
+			}
+			return rowSpans;
+		},
+		timetableRowSpan: function() {
+			let prevCountdown = null;
+			let timetableRowSpan = this.timetable;
+			for (let index in timetableRowSpan) {
+				if (timetableRowSpan[index].countdown == prevCountdown) { // seen date before
+					timetableRowSpan[index]['showDate'] = false;
+				} else {
+					timetableRowSpan[index]['showDate'] = true;
+					prevCountdown = timetableRowSpan[index].countdown;
+				}
+			}
+			return timetableRowSpan;
+		}
+	}
 }
 </script>
 
