9fb3b1cad8900569cd6e1431754d52f4b2b768ef
mitchellkwong <mitchellkwong@gmail.com>
2020-11-16 18:24:28 +0800

    Cleaned navbar before login

--- student_portal/src/components/app/Nav.vue
+++ student_portal/src/components/app/Nav.vue
@@ -17,7 +17,7 @@
 			</li>
 		</ul>
 		<ul v-else-if="right" class="nav justify-content-end">
-			<li align="left">{{ today }}</li>
+			<li v-if="semester" align="left">Semester {{ semester }}, {{ today }}</li>
 			<li class="nav-item" v-for="route in routes" :key="route.path">
 				<router-link 
 					:to="route.path"
