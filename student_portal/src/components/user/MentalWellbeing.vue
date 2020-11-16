27441c03c07cf4badb71bc8031f59fcefc01c654
mitchellkwong <mitchellkwong@gmail.com>
2020-11-16 17:34:19 +0800

    Re-added assets and fixed src issues

--- student_portal/src/components/user/MentalWellbeing.vue
+++ student_portal/src/components/user/MentalWellbeing.vue
@@ -1,7 +1,27 @@
 <template>
 	<div id='mentalWellbeing'>
-		<h1>Time for a break</h1>
-		<p>User "{{ user }}" is logged in.</p>
+		<h1>Feeling stressed during the pandemic?</h1>
+		
+		<div class="row bm-3 justify-content-center">
+			<img :src="require('@/assets/covid-anxiety.jpg')">
+		</div>
+
+		<h2>Check out our selection of articles below.</h2>
+
+		<div>
+			<figure>
+				<a href="https://www.nm.org/about-us/northwestern-medicine-newsroom/nm-news-blog/13-ways-to-help-a-friend-with-anxiety-during-coronavirus"><img v-img :src="require('@/assets/call.jpg')" width="250px"></a>
+				<figcaption>Call a friend</figcaption>
+			</figure>
+			<figure>
+				<a href="https://www.huffpost.com/entry/benefits-of-journaling-_b_6648884"><img v-img :src="require('@/assets/journal.png')" width="250px"></a>
+				<figcaption>Benefits of journalling</figcaption>
+			</figure>
+			<figure>
+				<a href="https://www.triathlete.com/training/yoga-during-the-pandemic/"><img v-img :src="require('@/assets/yoga.jpg')" width="250px"></a>
+				<figcaption>Pick up yoga</figcaption>
+			</figure>
+		</div>
 	</div>
 </template>
 
